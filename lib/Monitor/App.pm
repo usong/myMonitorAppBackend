785ea0bcee721ae5c12de647259a7a810ffdb9f2
usong <ricky.usong@gmail.com>
2013-04-21 19:11:19 +0800

    commit

--- lib/Monitor/App.pm
+++ lib/Monitor/App.pm
@@ -1,22 +1,49 @@
 package Monitor::App;
 use Dancer ':syntax';
+use Dancer::Plugin::Ajax;
 use Util::Basic;
 use File::Spec ;
+use Data::Dump qw(dump);
+use Encode;
+use utf8;
 our $VERSION = '0.1';
 
 set 'views'  => path( Util::Basic->proot, 'templates' );
 set 'layout'  => path( Util::Basic->proot, 'templates' );
 set 'public' => path( Util::Basic->proot, 'public' );
+set serializer => 'JSON';
 
 hook before_template_render => sub {
     my $tokens = shift;
 	$tokens->{public_resource} =  request->uri_base ;
-    #$tokens->{public_resource} = File::Spec->catdir( Util::Basic->proot , 'public' );
 };
-
-
-get '/' => sub {
+any '/' => sub {
     template 'nodes.tt2';
 };
 
+#any['get','post'] => '/get_value' => sub {
+options '/get_value' => sub {
+		header('Access-Control-Allow-Origin' => '*,');
+		header('Access-Control-Allow-Methods'=>'POST, GET, OPTIONS');
+		header('Access-Control-Allow-Headers'=>'origin, X-Requested-With, content-type, accept');
+};
+any['get','post'] => '/get_value' => sub {
+	
+	if ( request->request_method =~ /^options$/i ) {
+	    header('Access-Control-Allow-Origin' => '*,');
+		headers('Access-Control-Allow-Methods'=>'POST, GET, OPTIONS');
+		headers('Access-Control-Allow-Headers'=>'origin, X-Requested-With, content-type, accept');
+	} else {
+		my $json = JSON->new->utf8;
+	    my $json_text1 = param "XForms:Model";
+        $json_text1 = Encode::encode('UTF-8', $json_text1);
+		my $hash = $json->decode( $json_text1 ); 
+		header('Access-Control-Allow-Origin' => '*,');
+		header('Access-Control-Allow-Methods'=>'POST, GET, OPTIONS');
+		header('Access-Control-Allow-Headers'=>'origin, X-Requested-With, content-type, accept');
+		return $hash->{'ip'};
+	}
+};
 true;
+
+
